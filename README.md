618459b9d9eff087c034921b7fe3db10c2e429fd
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2022-04-25 20:13:23 +0900

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+## Blender Add-on: MakeWebp
+
+Make webp file.
+
+## Installation
+
+- Download https://github.com/SaitoTsutomu/MakeWebp/archive/refs/heads/main.zip
+- Start Blender.
+- Edit menu -> Preferences
+  - Select the "Add-ons" tab.
+  - Press the "Install ...".
+  - Select the downloaded ZIP file and press the button "Install Add-on".
+  - Check the "Object: MakeWebp".
+
+### Install Pillow
+
+This add-on needs Pillow.
+
+on macOS
+```
+/Applications/Blender.app/Contents/Resources/3.1/python/bin/python3.10 -m pip install Pillow
+```
+
+on Windows
+```
+"C:\Program Files\Blender Foundation\Blender 3.1\3.1\python\bin\python" -m pip install Pillow
+```
+
+## Usage
+
+- Select the Object Menu -> Make Webp
+- Create the webp.webp in the output directory.
---
+++ __init__.py
@@ -0,0 +1,60 @@
+import tempfile
+from pathlib import Path
+
+import bpy
+from PIL import Image
+
+bl_info = {
+    "name": "MakeWebp",
+    "author": "tsutomu",
+    "version": (0, 1),
+    "blender": (3, 1, 0),
+    "support": "TESTING",
+    "category": "Object",
+    "description": "",
+    "location": "View3D > Object",
+    "warning": "",
+    "doc_url": "https://github.com/SaitoTsutomu/MakeWebp",
+}
+
+
+class CMW_OT_make_webp(bpy.types.Operator):
+    bl_idname = "object.make_webp"
+    bl_label = "Make Webp"
+    bl_description = "Make webp file."
+
+    def execute(self, context):
+        scene = context.scene
+        pre_filepath = scene.render.filepath
+        pre_format = scene.render.image_settings.file_format
+        with tempfile.TemporaryDirectory() as tmpdir:
+            scene.render.filepath = str(Path(tmpdir) / "_")[:-1]
+            scene.render.image_settings.file_format = "JPEG"
+            bpy.ops.render.render(animation=True)
+            scene.render.filepath = pre_filepath
+            scene.render.image_settings.file_format = pre_format
+            imgs = [Image.open(f) for f in Path(tmpdir).glob("*.jpg")]
+            imgs[0].save(
+                Path(pre_filepath) / "webp.webp",
+                save_all=True,
+                append_images=imgs[1:],
+            )
+        return {"FINISHED"}
+
+
+def draw_item(self, context):
+    self.layout.operator(CMW_OT_make_webp.bl_idname)
+
+
+def register():
+    bpy.utils.register_class(CMW_OT_make_webp)
+    bpy.types.VIEW3D_MT_object.append(draw_item)
+
+
+def unregister():
+    bpy.utils.unregister_class(CMW_OT_make_webp)
+    bpy.types.VIEW3D_MT_object.remove(draw_item)
+
+
+if __name__ == "__main__":
+    register()
